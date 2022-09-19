b9e487d95847ccb563d10e14c1133abe5f5adb24
Ragul S V <s.v.ragul98@gmail.com>
2022-09-19 16:50:35 +0530

    Update bindable layout issue reproducing sample.

---
+++ BindableLayoutIssue/BindableLayoutIssue/IssuePage.xaml.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace BindableLayoutIssue
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class IssuePage : ContentPage
+    {
+        public IssuePage()
+        {
+            InitializeComponent();
+        }
+    }
+
+    public class Model : INotifyPropertyChanged
+    {
+        public ObservableCollection<string> colors;
+        public ObservableCollection<string> Colors
+        {
+            get
+            {
+                return colors;
+            }
+
+            set
+            {
+                colors = value;
+                OnPropertyChanged("Colors");
+            }
+        }
+
+        #region INotifyPropertyChanged
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            var changed = PropertyChanged;
+            if (changed == null)
+                return;
+
+            changed.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        #endregion
+
+
+    }
+
+    public class ViewModel
+    {
+        public Model Model { get; set; }
+
+        public ViewModel()
+        {
+            Model = new Model
+            {
+                Colors = new ObservableCollection<string>()
+                {
+                    "Cyan", "Yellow", "Orange","Cyan", "Yellow", "Orange","Cyan", "Yellow", "Orange","Cyan", 
+                    "Yellow", "Orange","Cyan", "Yellow", "Orange","Cyan", "Yellow", "Orange","Cyan", "Yellow",
+                    "Orange","Cyan", "Yellow", "Orange","Cyan", "Yellow", "Orange","Cyan", "Yellow", "Orange",
+                    "Cyan", "Yellow", "Orange","Cyan", "Yellow", "Orange","Cyan", "Yellow", "Orange","Cyan", 
+                    "Yellow", "Orange","Cyan", "Yellow", "Orange","Cyan", "Yellow", "Orange","Cyan", "Yellow",
+                    "Orange","Cyan", "Yellow", "Orange","Cyan", "Yellow", "Orange","Cyan", "Yellow", "Orange",
+                    "Cyan", "Yellow", "Orange","Cyan", "Yellow", "Orange","Cyan", "Yellow", "Orange","Cyan", 
+                    "Yellow", "Orange","Cyan", "Yellow", "Orange","Cyan", "Yellow", "Orange","Cyan", "Yellow", 
+                    "Orange","Cyan", "Yellow", "Orange","Cyan", "Yellow", "Orange","Cyan", "Yellow", "Orange",
+                    "Cyan", "Yellow", "Orange","Cyan", "Yellow", "Orange","Cyan", "Yellow", "Orange","Cyan", 
+                    "Yellow", "Orange","Cyan", "Yellow", "Orange","Cyan", "Yellow", "Orange","Cyan", "Yellow",
+                    "Orange","Cyan", "Yellow", "Orange","Cyan", "Yellow", "Orange","Cyan", "Yellow", "Orange",
+                    "Cyan", "Yellow", "Orange","Cyan", "Yellow", "Orange","Cyan", "Yellow", "Orange","Cyan", 
+                    "Yellow","Orange","Cyan", "Yellow", "Orange","Cyan", "Yellow", "Orange","Cyan", "Yellow", 
+                    "Orange", "Cyan", "Yellow", "Orange","Cyan", "Yellow", "Orange","Cyan", "Yellow", "Orange", 
+                    "Yellow", "Orange","Cyan", "Yellow", "Orange","Cyan", "Yellow", "Orange","Cyan", "Yellow", 
+                    "Orange","Cyan", "Yellow", "Orange","Cyan", "Yellow", "Orange","Cyan", "Yellow", "Orange",
+                    "Cyan", "Yellow", "Orange","Cyan", "Yellow", "Orange","Cyan", "Yellow", "Orange","Cyan", 
+                    "Yellow", "Orange","Cyan", "Yellow", "Orange","Cyan", "Yellow", "Orange","Cyan", "Yellow", 
+                    "Orange","Cyan", "Yellow", "Orange","Cyan", "Yellow", "Orange","Cyan", "Yellow", "Orange",
+                    "Cyan", "Yellow", "Orange","Cyan", "Yellow", "Orange","Cyan", "Yellow", "Orange","Cyan", 
+                    "Yellow", "Orange","Cyan", "Yellow", "Orange","Cyan", "Yellow", "Orange","Cyan", "Yellow", 
+                    "Orange","Cyan", "Yellow", "Orange","Cyan", "Yellow", "Orange","Cyan", "Yellow", "Orange",
+                    "Orange","Cyan", "Yellow", "Orange","Cyan", "Yellow", "Orange","Cyan", "Yellow", "Orange",
+                    "Cyan", "Yellow", "Orange","Cyan", "Yellow", "Orange","Cyan", "Yellow", "Orange","Cyan",
+                    "Yellow", "Orange","Cyan", "Yellow", "Orange","Cyan", "Yellow", "Orange","Cyan", "Yellow",
+                    "Orange","Cyan", "Yellow", "Orange","Cyan", "Yellow", "Orange","Cyan", "Yellow", "Orange",
+                    "Cyan", "Yellow", "Orange","Cyan", "Yellow", "Orange","Cyan", "Yellow", "Orange","Cyan",
+                    "Yellow", "Orange","Cyan", "Yellow", "Orange","Cyan", "Yellow", "Orange","Cyan", "Yellow",
+                    "Orange","Cyan", "Yellow", "Orange","Cyan", "Yellow", "Orange","Cyan", "Yellow", "Orange",
+                }
+            };
+        }
+    }
+}
